911b67ff13080cf98905a42aa1d8aa44f1839490
Kenneth Lim <kennethlimjf@gmail.com>
2015-01-27 14:43:13 +0800

    Made Response a usable object on its own

--- lib/urbanairship/response.rb
+++ lib/urbanairship/response.rb
@@ -2,13 +2,13 @@ module Urbanairship
   class Response
     attr_accessor :ok, :body, :push_ids, :code
 
-    def initialize(response)
-      unless response.body.nil?
+    def initialize(response=nil)
+      unless response.nil? || response.body.nil?
         @body = JSON.parse(response.body)
         @push_ids = @body["push_ids"]
         @ok = @body["ok"]
+        @code = response.code
       end
-      @code = response.code
     end
   end
 end
